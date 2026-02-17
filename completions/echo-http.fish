93309f0df540938703d7f71fb6f3c3182c86e171
Dmitry Verkhoturov <paskal.07@gmail.com>
2026-02-17 20:10:35 +0000

    use verbose completions for zsh and fish descriptions

--- completions/echo-http.fish
+++ completions/echo-http.fish
@@ -1,2 +1,2 @@
 # fish completion for echo-http (generated via go-flags)
-complete -c echo-http -a '(GO_FLAGS_COMPLETION=1 echo-http (commandline -cop) 2>/dev/null)'
+complete -c echo-http -a '(GO_FLAGS_COMPLETION=verbose echo-http (commandline -cop) 2>/dev/null | string replace -r "\\s+# " "\t")'
